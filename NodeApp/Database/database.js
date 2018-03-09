b5565dfe5d2e096c085ed6d44ab6dac852f13ede
Bryce Morrow <brycemorrow4564@gmail.com>
2018-03-08 22:40:27 -0500

    quick fix

--- NodeApp/Database/database.js
+++ NodeApp/Database/database.js
@@ -25,7 +25,7 @@ const enterData = (data, dataSource) => {
     dbKeyMapper.run(data, dataSource);
 };
 
-const min = (a,b) => {return a < b ? a : b; }
+const max = (a,b) => {return a > b ? a : b; }
 
 const cleanDatabase = () => {
     console.log("CLEANING THE DATABASE");
@@ -41,7 +41,7 @@ const cleanDatabase = () => {
         tablenames.forEach((tablename) => {
             pool.query(`SELECT * FROM ${tablename}`, [], (err, response) => {
                 let {rows} = response;
-                let mostRecentTime = min(parseInt(rows[0].date), parseInt(rows[rows.length-1].date));
+                let mostRecentTime = max(parseInt(rows[0].date), parseInt(rows[rows.length-1].date));
                 console.log("Most recent time");
                 console.log(mostRecentTime);
                 let cutoffTime = mostRecentTime - numDaysForRelevantData * (24 * 60 * 60 * 1000 * 1.0);
